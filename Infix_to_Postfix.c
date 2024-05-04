1b023499451a1b503fb06f6cf7168e759a6694cb
Shuvom Dhar <shuvomdhar8@gmail.com>
2024-05-05 00:06:34 +0530

    Add files via upload

---
+++ Infix_to_Postfix.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char push(struct stack *ptr, char val)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow!!");
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+char pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow!!");
+    }
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int stackTop(struct stack *sp)
+{
+    return sp->arr[sp->top];
+}
+
+int precedence(char ch)
+{
+    if (ch == '*' || ch == '/')
+    {
+        return 2;
+    }
+    else if (ch == '+' || ch == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isOperator(char ch)
+{
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char *InfixToPostfix(char *infix)
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc((sp->size) * (sizeof(char)));
+    char *postfix = (char *)malloc((strlen(infix) + 1) * (sizeof(char)));
+    int i = 0; // Track infix traversal
+    int j = 0; // Track postfix addition
+    while (infix[i] != '\0')
+    {
+        if (!isOperator(infix[i]))
+        {
+            postfix[j] = infix[i];
+            j++;
+            i++;
+        }
+        else
+        {
+            if (precedence(infix[i]) > precedence(stackTop(sp)))
+            {
+                push(sp, infix[i]);
+                i++;
+            }
+            else
+            {
+                postfix[j] = pop(sp);
+                j++;
+            }
+        }
+    }
+    while (!isEmpty(sp))
+    {
+        postfix[j] = pop(sp);
+        j++;
+    }
+    postfix[j] = '\0';
+    return postfix;
+}
+
+int main()
+{
+    char *infix = "x-y/z-k*d";
+    printf("Postfix is %s", InfixToPostfix(infix));
+    return 0;
+}
