13b3cdbe1743d09856b8e0ca044c46ec39ff42d3
Shuvom Dhar <shuvomdhar8@gmail.com>
2024-05-05 00:04:00 +0530

    Add files via upload

---
+++ Different_parenthesis_Match.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+
+// Function to check whether the stack is empty or not
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Function to check whether the stack is full or not
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void Push(struct stack *ptr, char value)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow! Cannot push %c to the stack\n", value);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+
+char Pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else
+    {
+        char value = ptr->arr[ptr->top];
+        ptr->top--;
+        return value;
+    }
+}
+
+int match(char a, char b)
+{
+    if (a == '(' && b == ')')
+    {
+        return 1;
+    }
+    if (a == '{' && b == '}')
+    {
+        return 1;
+    }
+    if (a == '[' && b == ']')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int parenthesisMatch(char *exp)
+{
+    // Create and initialize the stack
+    struct stack *sp;
+    sp->size = strlen(exp);
+    printf("The length of the expression is %d\n", sp->size);
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+        {
+            Push(sp, exp[i]);
+        }
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (isEmpty(sp))
+            {
+                return 0;
+            }
+            char popped_ch = Pop(sp);
+            if (!match(popped_ch, exp[i]))
+            {
+                return 0;
+            }
+        }
+    }
+    if (isEmpty(sp))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    char *exp = "[(8*9)-{(12/6)*4+3}]";
+    // Check whether the stack is empty or not
+    if (parenthesisMatch(exp))
+    {
+        printf("The parenthesis is matching\n");
+    }
+    else
+    {
+        printf("The parenthesis is not matching\n");
+    }
+    return 0;
+}
